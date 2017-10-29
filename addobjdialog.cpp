7b8f9bdcfaaa175b7499e5e6740938c21b2c0477
floverina <33018103+floverina@users.noreply.github.com>
2017-10-29 12:34:08 +0200

    Update addobjdialog.cpp

--- addobjdialog.cpp
+++ addobjdialog.cpp
@@ -1,94 +1,44 @@
-#include <addobjdialog.h>
+#ifndef ADDOBJDIALOG_H
+#define ADDOBJDIALOG_H
 
-AddObjDialog::AddObjDialog(QWidget *parent) :
-    QDialog(parent) {
-    this->setWindowTitle("Добавить задачу");
-    setWindowFlags (windowFlags() & ~Qt::WindowContextHelpButtonHint);
+#include <QWidget>
+#include <QtWidgets>
 
-    InitializeComponent();
-    SetupLayouts();
-    ConnectSignals();
-}
+class AddObjDialog : public QDialog {
+    Q_OBJECT
+private:
+    QPushButton* pbSave;
+    QPushButton* pbCancel;
 
-void AddObjDialog::InitializeComponent() {
-    pbSave = new QPushButton("Сохранить");
-    pbCancel = new QPushButton("Отмена");
+    QLabel* lName;
+    QLabel* lDate;
+    QLabel* lImportance;
+    QLabel* lTag;
+    QLabel* lRegular;
+    QLabel* lDescription;
+    QLabel* lNotification;
 
-    lName = new QLabel("&Название:");
-    leName = new QLineEdit();
-    lName->setBuddy(leName);
-
-    lDate = new QLabel("&Выполнить до:");
-    deDate = new QDateEdit(QDate::currentDate().addDays(1));
-    teDate = new QTimeEdit(QTime(23, 0));
-    lDate->setBuddy(lDate);
-
-
-    lImportance = new QLabel("&Приоритет:");
-    sbImportance = new QSpinBox();
-    sbImportance->setRange(1, 5);
-    sbImportance->setWrapping(true);
-    lImportance->setBuddy(sbImportance);
-
-    lTag = new QLabel("&Тэг:");
-    leTag = new QLineEdit();
-    lTag->setBuddy(leTag);
-
-    lRegular = new QLabel("&Повторять");
-    //тут че-то
-
-    lDescription = new QLabel("&Описание:");
-    teDescription = new QPlainTextEdit();
-    teDescription->setFixedHeight(50);
-    lDescription->setBuddy(teDescription);
-
-    lNotification = new QLabel("&Уведомлять:");
-    cbNotification = new QComboBox();
-    QStringList tmp;
-    tmp << "Нет" << "В день события" << "За три дня" << "За неделю";
-    cbNotification->addItems(tmp);
-    lNotification->setBuddy(cbNotification);
+    QLineEdit* leName;
+    QDateEdit* deDate;
+    QTimeEdit* teDate;
+    QSpinBox* sbImportance;
+    QLineEdit* leTag;
+    //повтор
+    QPlainTextEdit* teDescription;
+    QComboBox* cbNotification;
 
     //Layouts
-    mlay = new QVBoxLayout();
-    layForButton = new QHBoxLayout();
-    layForDate = new QHBoxLayout();
-}
+    QVBoxLayout* mlay;
+    QHBoxLayout* layForButton;
+    QHBoxLayout* layForDate;
+public:
+    AddObjDialog(QWidget *parent = 0);
+    void InitializeComponent();
+    void SetupLayouts();
+    void ConnectSignals();
+public slots:
 
-void AddObjDialog::SetupLayouts() {
-    //add
-    layForButton->addStretch();
-    layForButton->addWidget(pbSave);
-    layForButton->addWidget(pbCancel);
 
-    layForDate->addWidget(deDate);
-    layForDate->addWidget(teDate);
+};
 
-    mlay->addWidget(lName);
-    mlay->addWidget(leName);
-
-    mlay->addWidget(lDate);
-    mlay->addLayout(layForDate);
-
-    mlay->addWidget(lImportance);
-    mlay->addWidget(sbImportance);
-
-    mlay->addWidget(lTag);
-    mlay->addWidget(leTag);
-
-    mlay->addWidget(lRegular);
-
-    mlay->addWidget(lDescription);
-    mlay->addWidget(teDescription);
-
-    mlay->addLayout(layForButton);
-
-    //set
-    setLayout(mlay);
-}
-
-void AddObjDialog::ConnectSignals() {
-    connect(pbSave, SIGNAL(clicked(bool)), SLOT(accept()));
-    connect(pbCancel, SIGNAL(clicked(bool)), SLOT(reject()));
-    //connect(cbNotification, SIGNAL(currentIndexChanged(QString)), SLOT()
-}
+#endif // ADDOBJDIALOG_H
